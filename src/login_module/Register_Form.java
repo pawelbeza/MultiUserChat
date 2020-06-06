142526e57612796ce18f0d87b698fa6efc4b5b6c
Paweł Bęza <pawel.beza99@gmail.com>
2020-06-06 19:07:39 +0200

    fixup! Migration from Netbeans to Intellij

--- src/login_module/Register_Form.java
+++ src/login_module/Register_Form.java
@@ -81,10 +81,7 @@ public class Register_Form extends javax.swing.JFrame {
             new javax . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e"
             , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM
             , new java. awt .Font ( "D\u0069al\u006fg", java .awt . Font. BOLD ,12 )
-            ,java . awt. Color .red ) ,jPanel1. getBorder () ) ); jPanel1. addPropertyChangeListener(
-            new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e
-            ) { if( "\u0062or\u0064er" .equals ( e. getPropertyName () ) )throw new RuntimeException( )
-            ;} } );
+            ,java . awt. Color .red ) ,jPanel1. getBorder () ) );
 
             //======== jPanel2 ========
             {
